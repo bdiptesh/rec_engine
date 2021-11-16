56ddbf39fb56071405c4bc22e0ce1a861f313272
Deepak <dkumar6311@gmail.com>
2021-11-16 17:20:15 +0530

    V0.1.0

--- rec/__main__.py
+++ rec/__main__.py
@@ -8,10 +8,8 @@ Credits
     Date: Nov 15, 2021
 """
 
-from lib.rec_engine import RecommendationFun, pd # pylint: disable=E0401
-df_catalog=pd.read_csv\
-        ("../data/input/catalog.csv")
-df_uid=pd.read_csv\
-        ("../data/input/users.csv")
-reco = RecommendationFun(df_catalog,df_uid,3)
+from lib.rec_engine import RecommendationFun, pd  # pylint: disable=E0401
+df_catalog = pd.read_csv("../data/input/catalog.csv")
+df_uid = pd.read_csv("../data/input/users.csv")
+reco = RecommendationFun(df_catalog, df_uid, 3)
 final_recommendation = reco.rec()
--- rec/lib/rec_engine.py
+++ rec/lib/rec_engine.py
@@ -5,7 +5,7 @@ This is a temporary script file.
 """
 import pandas as pd
 import numpy as np
-class RecommendationFun(): # pylint: disable=R0903
+class RecommendationFun():  # pylint: disable=R0903
     """Short summary.
 
     Parameters
@@ -31,14 +31,17 @@ class RecommendationFun(): # pylint: disable=R0903
     should illustrate how to use the function/class.
     >>>
     """
-    def __init__(self, df_cat, df_user,k : int = 3):
+    def __init__(self, df_cat, df_user, k : int = 3):
         self.df_catalog = df_cat
         self.df_uid = df_user
         self.k = k
-        self.dict_catalog_pref= self.df_catalog.set_index(df_cat.columns[0]).T.to_dict('list')
-        self.dict_users_pref= self.df_uid.set_index(df_user.columns[0]).T.to_dict('list')
-        self.final1=None
-        self.final_list=None
+        self.dict_catalog_pref = self.df_catalog.set_index(df_cat.columns[0])\
+            .T.to_dict('list')
+        self.dict_users_pref = self.df_uid.set_index(df_user.columns[0]).\
+            T.to_dict('list')
+        self.final1 = None
+        self.final_list = None
+
     def rec(self):
         """Short summary.
         Returns
@@ -55,27 +58,28 @@ class RecommendationFun(): # pylint: disable=R0903
         should illustrate how to use the function/class.
         >>>
         """
-        self.final1=[]
+        self.final1 = []
         for key1, values1 in self.dict_users_pref.items():
             user_id = key1
             prefs = values1
             for key, values in self.dict_catalog_pref.items():
                 rest_id = key
                 rest_catalog = values
-                i=0
-                score=0
-                list1=[]
+                i = 0
+                score = 0
+                list1 = []
                 for i in np.arange(0, len(prefs)):
-                    if (prefs[i]==rest_catalog[i]) and (prefs[i]==1):
-                        score=score+1
-                        score_perc=(score/sum(prefs))
+                    if (prefs[i] == rest_catalog[i]) and (prefs[i] == 1):
+                        score = score+1
+                        score_perc = (score/sum(prefs))
                 list1.append(user_id)
                 list1.append(rest_id)
                 list1.append(score)
                 list1.append(score_perc)
                 self.final1.append(list1)
-        df_pre_final=pd.DataFrame(self.final1,columns=['User_id','Rest_id','Match','Score'])
-        df_final=df_pre_final.sort_values\
-        (['User_id','Score'],ascending=[True, False]).groupby('User_id').head(self.k)
-        self.final_list=df_final.values.tolist()
+        df_pre_final = pd.DataFrame(self.final1,
+                                    columns = ['User_id', 'Rest_id', 'Match', 'Score'])
+        df_final = df_pre_final.sort_values\
+            (['User_id', 'Score'], ascending = [ True, False]).groupby('User_id').head(self.k)
+        self.final_list = df_final.values.tolist()
         return self.final_list
