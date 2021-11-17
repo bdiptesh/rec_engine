27fbf18be72bc40e6150fb9aa60bae33688e04b5
Deepak <dkumar6311@gmail.com>
2021-11-17 10:30:16 +0530

    Update __main__.py

--- rec/__main__.py
+++ rec/__main__.py
@@ -9,17 +9,15 @@ Credits
 """
 
 import time
-from lib import utils # pylint: disable=E0401
-from lib.rec_engine import RecommendationFun, pd # pylint: disable=E0401
+from lib import utils  # pylint: disable=E0401
+from lib.rec_engine import RecommendationFun, pd  # pylint: disable=E0401
 
 SEP = '-' * 70
 ELAPSED_TIME = utils.elapsed_time
 
 START = time.time_ns()
-df_catalog=pd.read_csv\
-        ("../data/input/catalog.csv")
-df_uid=pd.read_csv\
-        ("../data/input/users.csv")
-reco = RecommendationFun(df_catalog,df_uid,3)
+df_catalog = pd.read_csv("../data/input/catalog.csv")
+df_uid = pd.read_csv("../data/input/users.csv")
+reco = RecommendationFun(df_catalog, df_uid, 3)
 final_recommendation = reco.rec()
 print(SEP, ELAPSED_TIME("Total time", START), SEP, sep="\n")
